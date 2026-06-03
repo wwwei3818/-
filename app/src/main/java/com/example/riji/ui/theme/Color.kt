aea27cb7284d42e8a8935da951da05790f233f16
wwwei3818 <3258215227@qq.com>
2026-06-03 16:46:13 +0800

    feat: update color system to black-white + macaron accent

--- app/src/main/java/com/example/riji/ui/theme/Color.kt
+++ app/src/main/java/com/example/riji/ui/theme/Color.kt
@@ -2,29 +2,29 @@ package com.example.riji.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Light Mode - Cream/Warm white style
-val LightBackground = Color(0xFFF5F5F0)
-val LightCardBackground = Color(0xFFFFFFFF)
-val LightOnBackground = Color(0xFF1A1A1A)
-val LightOnCard = Color(0xFF888888)
+// Light Mode - Black & White main style
+val LightBackground = Color(0xFFF5F5F0) // 浅灰白
+val LightCardBackground = Color(0xFFFFFFFF) // 纯白
+val LightOnBackground = Color(0xFF1A1A1A) // 深黑
+val LightOnCard = Color(0xFF666666) // 中灰
 val LightDivider = Color(0xFFEEEEEE)
 val LightSurfaceVariant = Color(0xFFF0F0EB)
 
-// Dark Mode
-val DarkBackground = Color(0xFF1A1A1A)
-val DarkCardBackground = Color(0xFF2A2A2A)
-val DarkOnBackground = Color(0xFFFFFFFF)
-val DarkOnCard = Color(0xFF999999)
+// Dark Mode - Black & White main style
+val DarkBackground = Color(0xFF121212) // 纯黑
+val DarkCardBackground = Color(0xFF1E1E1E) // 深灰
+val DarkOnBackground = Color(0xFFFFFFFF) // 纯白
+val DarkOnCard = Color(0xFF999999) // 浅灰
 val DarkDivider = Color(0xFF333333)
-val DarkSurfaceVariant = Color(0xFF222222)
+val DarkSurfaceVariant = Color(0xFF1A1A1A)
 
-// Macaron Accent Colors
-val PinkAccent = Color(0xFFFFB5B5)
-val BlueAccent = Color(0xFFB5D8FF)
-val GreenAccent = Color(0xFFB5FFD8)
-val YellowAccent = Color(0xFFFFE5B5)
-val PurpleAccent = Color(0xFFD8B5FF)
-val OrangeAccent = Color(0xFFFFD8B5)
+// Macaron Accent Colors (低饱和马卡龙)
+val PinkAccent = Color(0xFFE8C4C4) // 低饱和粉
+val BlueAccent = Color(0xFFC4D8E8) // 低饱和蓝
+val GreenAccent = Color(0xFFC4E8D4) // 低饱和绿
+val YellowAccent = Color(0xFFE8DCC4) // 低饱和黄
+val PurpleAccent = Color(0xFFD4C4E8) // 低饱和紫
+val OrangeAccent = Color(0xFFE8D4C4) // 低饱和橙
 
 // Functional Colors
 val SuccessColor = Color(0xFF4CAF50)
@@ -32,16 +32,16 @@ val WarningColor = Color(0xFFFF9800)
 val ErrorColor = Color(0xFFF44336)
 val InfoColor = Color(0xFF2196F3)
 
-// Category Colors
-val LifeColor = Color(0xFFFFB5B5)
-val WorkColor = Color(0xFFB5D8FF)
-val CoupleColor = Color(0xFFFFD8B5)
-val TravelColor = Color(0xFFB5FFD8)
-val StudyColor = Color(0xFFD8B5FF)
+// Category Colors (低饱和马卡龙)
+val LifeColor = Color(0xFFE8C4C4) // 生活-低饱和粉
+val WorkColor = Color(0xFFC4D8E8) // 工作-低饱和蓝
+val CoupleColor = Color(0xFFE8D4C4) // 恋爱-低饱和橙
+val TravelColor = Color(0xFFC4E8D4) // 旅行-低饱和绿
+val StudyColor = Color(0xFFD4C4E8) // 学习-低饱和紫
 
-// Card accent colors for different types
-val AnniversaryCardBg = Color(0xFFFFF8F0)
-val CheckInCardBg = Color(0xFFF0FFF8)
-val AssetCardBg = Color(0xFFF8F8FF)
-val PlanCardBg = Color(0xFFFFF0F8)
-val DiaryCardBg = Color(0xFFFFF8FF)
+// Card accent colors for different types (低饱和)
+val AnniversaryCardBg = Color(0xFFF5F0E8) // 纪念日-低饱和暖
+val CheckInCardBg = Color(0xFFE8F5F0) // 打卡-低饱和绿
+val AssetCardBg = Color(0xFFF0F0F5) // 资产-低饱和蓝
+val PlanCardBg = Color(0xFFF5E8F0) // 计划-低饱和粉
+val DiaryCardBg = Color(0xFFF0F0F8) // 日记-低饱和紫
